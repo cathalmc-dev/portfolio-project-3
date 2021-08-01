662b0641e624a2269d38c6eaa6a934c6c2f4ce88
cathalmc-dev <cathalmc.dev@gmail.com>
2021-08-01 18:19:40 +0000

    Adjust store identification function and include python debegging functionality

--- run.py
+++ run.py
@@ -11,16 +11,35 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 
-def identify_user():
+
+def identify_store():
     store = input("Enter your store name: ").lower()
     workbook = GSPREAD_CLIENT.open(store)
+    print(workbook)
+    print(dir(workbook))
+    import pdb; pdb.set_trace()
+    if store != "dundrum":
+        print("Invalid")
+        identify_store()
+    else:
+        identify_user()
+
+
+def identify_user():
     user = input("Enter your user name: ").lower()
-    sheet = workbook.worksheet(user)
-    data = sheet.get_all_values()
-    print(data)
+    print(user)
+    # if validate_user(user):
+    #     sheet = workbook.worksheet(user)
+    #     data = sheet.get_all_values()
+    #     print(data)
+
+
+# def validate_user():
+#     try:
+
 
 def main():
-    identify_user()
+    identify_store()
 
 print("Welcome to Mobile Commission Tracker")
 main()
